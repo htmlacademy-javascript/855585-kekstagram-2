41542ac284f0847843965985194b93dbb7622d57
Keks <keks@htmlacademy.ru>
2024-12-25 12:54:05 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/modal.js
@@ -0,0 +1,63 @@
+import {isEscapeKey} from './util.js';
+import {renderBigPicture} from './full-size-picture.js';
+import {clearComments} from './comments.js';
+
+const renderModal = (posts) => {
+  const bigPictureElement = document.body.querySelector('.big-picture');
+  const picturesParentElement = document.querySelector('.pictures');
+  const pictureCloseButton = bigPictureElement.querySelector('.big-picture__cancel');
+
+
+  //Функция-обработчик закрытия модалки ESC
+  const onModalEscKeyDown = (evt) => {
+    if (isEscapeKey(evt)) {
+      evt.preventDefault();
+      closeModal();
+    }
+  };
+
+  //Функция закрытия модалки
+  function closeModal () {
+    clearComments();
+    bigPictureElement.classList.add('hidden');
+    //Удаляем обработчик закрытия превью ESC
+    document.removeEventListener('keydown', onModalEscKeyDown);
+    //Удаляем класс,  чтобы контейнер с фото прокручивался
+    document.body.classList.remove('modal-open');
+  }
+
+  //Функция закрытия модалки по нажатию на кнопку
+  const onCloseButtonClick = (button) => {
+    button.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      closeModal();
+    });
+  };
+
+  onCloseButtonClick(pictureCloseButton);
+
+  //Функция открытия модалки
+  const openModal = () => {
+    bigPictureElement.classList.remove('hidden');
+    //Добавляем обработчик закрытия превью ESC
+    document.addEventListener('keydown', onModalEscKeyDown);
+    //Добавляем класс, чтобы контейнер с фото не прокручивался
+    document.body.classList.add('modal-open');
+  };
+
+  //Функция обработчика клика, показывает модалку
+  const onThumbnailClick = (evt) => {
+    const currentPictureElement = evt.target.closest('.picture');
+    if (currentPictureElement) {
+      evt.preventDefault();
+      //Находим элемент из массива постов с таким же id как у текущего элемента
+      const foundedPictureByIdElement = posts.find((picture) => picture.id === Number(currentPictureElement.dataset.pictureId));
+      renderBigPicture(foundedPictureByIdElement);
+      openModal();
+    }
+  };
+
+  picturesParentElement.addEventListener('click', onThumbnailClick);
+};
+
+export {renderModal};
---
+++ 9/js/util.js
@@ -0,0 +1,27 @@
+//Функция, возвращающая случайное число в заданном диапазоне (включительно)
+const getRandomInteger = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+//Функция, возвращающая уникальные значения из заданного диапазона
+const createUniqueRandomInteger = (min, max) => {
+  const previousValues = [];
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+};
+
+//Функция проверяющая клавиша esc или нет
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export {getRandomInteger, createUniqueRandomInteger, isEscapeKey};
